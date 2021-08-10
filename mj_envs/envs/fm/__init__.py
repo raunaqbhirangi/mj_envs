c6cbdf3ec65b342eb77a922e0c25a47fc63b4df4
Raunaq Bhirangi <raunaqbhirangi@gmail.com>
2021-08-10 15:38:28 -0400

    add dmanus environment

--- mj_envs/envs/fm/__init__.py
+++ mj_envs/envs/fm/__init__.py
@@ -26,3 +26,14 @@ register(
             'target_pose': np.array([0, 0, 0, -1.57, 0, 0, 0, 0, 1, 1, 0, 1, 1, 0, 1, 1]),
         }
 )
+
+register(
+    id='DManusReachFixed-v0',
+    entry_point='mj_envs.envs.fm.base_v0:DManusEnvFixed',
+    max_episode_steps=50, #50steps*40Skip*2ms = 4s
+    kwargs={
+            'model_path': '/assets/dmanus.xml',
+            'config_path': curr_dir+'/assets/dmanus.config',
+            'target_pose': np.array([0, 1, 1, 0, 1, 1, 0, 1, 1]),
+        }
+)
--- mj_envs/envs/fm/base_v0.py
+++ mj_envs/envs/fm/base_v0.py
@@ -5,7 +5,7 @@ from mj_envs.utils.xml_utils import reassign_parent
 import os
 import collections
 
-class FMBase(env_base.MujocoEnv):
+class DManusBase(env_base.MujocoEnv):
 
     DEFAULT_OBS_KEYS = [
         'qp', 'qv', 'pose_err'
@@ -20,33 +20,26 @@ class FMBase(env_base.MujocoEnv):
 
         curr_dir = os.path.dirname(os.path.abspath(__file__))
 
-        # Process model to ues DManus as end effector
-        raw_sim = env_base.get_sim(model_path=curr_dir+model_path)
-        raw_xml = raw_sim.model.get_xml()
-        processed_xml = reassign_parent(xml_str=raw_xml, receiver_node="panda0_link7", donor_node="DManus_mount")
-        processed_model_path = curr_dir+model_path[:-4]+"_processed.xml"
-        with open(processed_model_path, 'w') as file:
-            file.write(processed_xml)
-
-        self.sim = env_base.get_sim(model_path=processed_model_path)
-        self.sim_obsd = env_base.get_sim(model_path=processed_model_path)
-        os.remove(processed_model_path)
-
         self.target_pose = target_pose
-
-        # get env
+        
         env_base.MujocoEnv.__init__(self,
-                                sim = self.sim,
-                                sim_obsd = self.sim_obsd,
-                                frame_skip = 40,
-                                config_path = config_path,
-                                obs_keys = self.DEFAULT_OBS_KEYS,
-                                rwd_keys_wt = self.DEFAULT_RWD_KEYS_AND_WEIGHTS,
-                                rwd_mode = "dense",
-                                act_mode = "pos",
-                                act_normalized = True,
-                                is_hardware = False,
-                                **kwargs)
+                                curr_dir+model_path)
+        
+
+        env_base.MujocoEnv._setup(self, 
+            obs_keys=self.DEFAULT_OBS_KEYS,
+            weighted_reward_keys = self.DEFAULT_RWD_KEYS_AND_WEIGHTS,
+            reward_mode = "dense",             
+            frame_skip = 40,
+            normalize_act = True,
+            # obs_range = (-10, 10),
+            # seed = None,
+            act_mode = "pos",
+            is_hardware = False,
+            config_path = config_path,
+            # rwd_viz = False,
+            robot_name = 'fm', 
+        )
 
 
     def get_obs_dict(self, sim):
@@ -74,6 +67,102 @@ class FMBase(env_base.MujocoEnv):
         ))
         rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
         return rwd_dict
+
+class FMBase(env_base.MujocoEnv):
+
+    DEFAULT_OBS_KEYS = [
+        'qp', 'qv', 'pose_err'
+    ]
+    DEFAULT_RWD_KEYS_AND_WEIGHTS = {
+        "reach": -1.0,
+        "bonus": 4.0,
+        "penalty": -50,
+    }
+
+    def __init__(self, model_path, config_path, target_pose, **kwargs):
+
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+
+        # Process model to ues DManus as end effector
+        raw_sim = env_base.get_sim(model_path=curr_dir+model_path)
+        raw_xml = raw_sim.model.get_xml()
+        processed_xml = reassign_parent(xml_str=raw_xml, receiver_node="panda0_link7", donor_node="DManus_mount")
+        processed_model_path = curr_dir+model_path[:-4]+"_processed.xml"
+        with open(processed_model_path, 'w') as file:
+            file.write(processed_xml)
+
+        # self.sim = env_base.get_sim(model_path=processed_model_path)
+        # self.sim_obsd = env_base.get_sim(model_path=processed_model_path)
+        # os.remove(processed_model_path)
+
+        self.target_pose = target_pose
+
+        # get env
+        # env_base.MujocoEnv.__init__(self,
+        #                         # sim = self.sim,
+        #                         # sim_obsd = self.sim_obsd,
+        #                         frame_skip = 40,
+        #                         config_path = config_path,
+        #                         obs_keys = self.DEFAULT_OBS_KEYS,
+        #                         rwd_keys_wt = self.DEFAULT_RWD_KEYS_AND_WEIGHTS,
+        #                         rwd_mode = "dense",
+        #                         act_mode = "pos",
+        #                         act_normalized = True,
+        #                         is_hardware = False,
+        #                         **kwargs)
+        
+        env_base.MujocoEnv.__init__(self,
+                                processed_model_path)
+        
+        os.remove(processed_model_path)
+
+        env_base.MujocoEnv._setup(self, 
+            obs_keys=self.DEFAULT_OBS_KEYS,
+            weighted_reward_keys = self.DEFAULT_RWD_KEYS_AND_WEIGHTS,
+            reward_mode = "dense",             
+            frame_skip = 40,
+            normalize_act = True,
+            # obs_range = (-10, 10),
+            # seed = None,
+            act_mode = "pos",
+            is_hardware = False,
+            config_path = config_path,
+            # rwd_viz = False,
+            robot_name = 'fm', 
+        )
+
+
+    def get_obs_dict(self, sim):
+        obs_dict = {}
+        obs_dict['t'] = np.array([self.sim.data.time])
+        obs_dict['qp'] = sim.data.qpos.copy()
+        obs_dict['qv'] = sim.data.qvel.copy()
+        obs_dict['pose_err'] = obs_dict['qp'] - self.target_pose
+        return obs_dict
+
+
+    def get_reward_dict(self, obs_dict):
+        reach_dist = np.linalg.norm(obs_dict['pose_err'], axis=-1)
+        far_th = 10
+
+        rwd_dict = collections.OrderedDict((
+            # Optional Keys
+            ('reach',   reach_dist),
+            ('bonus',   (reach_dist<1) + (reach_dist<2)),
+            ('penalty', (reach_dist>far_th)),
+            # Must keys
+            ('sparse',  -1.0*reach_dist),
+            ('solved',  reach_dist<.5),
+            ('done',    reach_dist > far_th),
+        ))
+        rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
+        return rwd_dict
+
+class DManusEnvFixed(DManusBase):
+
+    def reset(self):
+        obs = super().reset(self.init_qpos, self.init_qvel)
+        return obs
 
 
 class FMReachEnvFixed(FMBase):
