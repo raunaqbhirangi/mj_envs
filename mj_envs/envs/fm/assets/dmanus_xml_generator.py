a8e4690c86273a6c9a04107c417192b262dd7be5
Raunaq Bhirangi <raunaqbhirangi@gmail.com>
2021-09-16 17:46:26 -0400

    add details to xml gen file

--- mj_envs/envs/fm/assets/dmanus_xml_generator.py
+++ mj_envs/envs/fm/assets/dmanus_xml_generator.py
@@ -1,5 +1,14 @@
+import os
+
 from mj_envs.utils.xml_utils import parse_xml_with_comments, get_xml_str
 import xml.etree.ElementTree as ET
+
+'''
+These functions are generally intended to reduce clutter in the xml
+assets. The idea is to be able to generate xml files on the fly with 
+specific configs, and not need to regenerate if there already exists an
+xml file with the same config
+'''
 
 def generate_xml_config(params_xml, params_config):
     return generate_xml(params_xml), generate_config_file(params_config)
@@ -8,12 +17,12 @@ def generate_config_file(params):
     '''
     Generates .config files with sensor and actuators for mujoco Robot instances
     '''
-    config = []
-    return config
+    path_to_config = './'
+    return path_to_config
     
 def generate_xml(params):
     '''
     Generates xml files with requisite parameters
     '''
-    xml = []
-    return xml
+    path_to_xml = ''
+    return path_to_xml
