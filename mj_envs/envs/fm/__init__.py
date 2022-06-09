38707224ebcf9d8c10f7a3aece8226fe425388bd
Raunaq Bhirangi <raunaqbhirangi@fb.com>
2022-06-09 17:56:14 -0400

    FrankaDManusPose with joint removal

--- mj_envs/envs/fm/__init__.py
+++ mj_envs/envs/fm/__init__.py
@@ -42,12 +42,13 @@ register(
 # Ball random target from oracle info
 register(
     id='rpFrankaDManusBallBalanceOracle-v0',
-    entry_point='mj_envs.envs.fm.franka_dmanus_pose_v0:FrankaDmanusPoseWithBall',
+    entry_point='mj_envs.envs.fm.franka_dmanus_pose_v0:FrankaDmanusPoseWithBallJointsRemoved',
     max_episode_steps=100, #50steps*40Skip*2ms = 4s
     # Franka init specifies the reset joint angles
     kwargs={
             'model_path': '/assets/franka_dmanus_ball.xml',
             'config_path': os.path.join(curr_dir,'assets/franka_dmanus.config'),
+            'removed_joint_ids': [1,2,3],
             'obs_keys': ['qp', 'qv', 'ball', 'dball'],
             'franka_init': np.array([-0.0005, 0.9809, 0.1050, -0.5971, 1.4641, 0.2593, 0.0022]),
             'target_xy_range': np.array(([0.0, 0.05], [0.0, 0.05])),
--- mj_envs/envs/fm/franka_dmanus_pose_v0.py
+++ mj_envs/envs/fm/franka_dmanus_pose_v0.py
@@ -8,12 +8,12 @@ License :: Under Apache License, Version 2.0 (the "License"); you may not use th
 import gym
 import numpy as np
 from mj_envs.envs import env_base
-from mj_envs.utils.xml_utils import reassign_parent
+from mj_envs.utils.xml_utils import reassign_parent, remove_joint
 import os
 import collections
 
-class FrankaDmanusPose(env_base.MujocoEnv):
 
+class FrankaDmanusPose(env_base.MujocoEnv):
     DEFAULT_OBS_KEYS = [
         'qp', 'qv', 'pose_err'
     ]
@@ -22,15 +22,16 @@ class FrankaDmanusPose(env_base.MujocoEnv):
         "bonus": 4.0,
         "penalty": -50,
     }
+
     def __init__(self, model_path, obsd_model_path=None, seed=None, **kwargs):
 
         curr_dir = os.path.dirname(os.path.abspath(__file__))
 
         # Process model to use DManus as end effector
-        raw_sim = env_base.get_sim(model_path=curr_dir+model_path)
+        raw_sim = env_base.get_sim(model_path=curr_dir + model_path)
         raw_xml = raw_sim.model.get_xml()
         processed_xml = reassign_parent(xml_str=raw_xml, receiver_node="panda0_link7", donor_node="DManus_mount")
-        processed_model_path = curr_dir+model_path[:-4]+"_processed.xml"
+        processed_model_path = curr_dir + model_path[:-4] + "_processed.xml"
         with open(processed_model_path, 'w') as file:
             file.write(processed_xml)
 
@@ -38,10 +39,10 @@ class FrankaDmanusPose(env_base.MujocoEnv):
         if obsd_model_path == model_path:
             processed_obsd_model_path = processed_model_path
         elif obsd_model_path:
-            raw_sim = env_base.get_sim(model_path=curr_dir+obsd_model_path)
+            raw_sim = env_base.get_sim(model_path=curr_dir + obsd_model_path)
             raw_xml = raw_sim.model.get_xml()
             processed_xml = reassign_parent(xml_str=raw_xml, receiver_node="panda0_link7", donor_node="DManus_mount")
-            processed_obsd_model_path = curr_dir+obsd_model_path[:-4]+"_processed.xml"
+            processed_obsd_model_path = curr_dir + obsd_model_path[:-4] + "_processed.xml"
             with open(processed_obsd_model_path, 'w') as file:
                 file.write(processed_xml)
         else:
@@ -61,23 +62,22 @@ class FrankaDmanusPose(env_base.MujocoEnv):
         # created in __init__ to complete the setup.
         super().__init__(model_path=processed_model_path, obsd_model_path=processed_obsd_model_path, seed=seed)
         os.remove(processed_model_path)
-        if processed_obsd_model_path and processed_obsd_model_path!=processed_model_path:
+        if processed_obsd_model_path and processed_obsd_model_path != processed_model_path:
             os.remove(processed_obsd_model_path)
 
         self._setup(**kwargs)
-
 
     def _setup(self,
                target_pose,
                obs_keys=DEFAULT_OBS_KEYS,
                weighted_reward_keys=DEFAULT_RWD_KEYS_AND_WEIGHTS,
                **kwargs):
-        if isinstance(target_pose,np.ndarray):
+        if isinstance(target_pose, np.ndarray):
             self.target_type = 'fixed'
             self.target_pose = target_pose
         elif target_pose == 'random':
             self.target_type = 'random'
-            self.target_pose = self.sim.data.qpos.copy() # fake target for setup
+            self.target_pose = self.sim.data.qpos.copy()  # fake target for setup
 
         super()._setup(obs_keys=obs_keys,
                        weighted_reward_keys=weighted_reward_keys,
@@ -92,78 +92,81 @@ class FrankaDmanusPose(env_base.MujocoEnv):
         obs_dict['pose_err'] = obs_dict['qp'] - self.target_pose
         return obs_dict
 
-
     def get_reward_dict(self, obs_dict):
         pose_dist = np.linalg.norm(obs_dict['pose_err'], axis=-1)
         far_th = 10
 
         rwd_dict = collections.OrderedDict((
             # Optional Keys
-            ('pose',   pose_dist),
-            ('bonus',   (pose_dist<1) + (pose_dist<2)),
-            ('penalty', (pose_dist>far_th)),
+            ('pose', pose_dist),
+            ('bonus', (pose_dist < 1) + (pose_dist < 2)),
+            ('penalty', (pose_dist > far_th)),
             # Must keys
-            ('sparse',  -1.0*pose_dist),
-            ('solved',  pose_dist<.5),
-            ('done',    pose_dist > far_th),
+            ('sparse', -1.0 * pose_dist),
+            ('solved', pose_dist < .5),
+            ('done', pose_dist > far_th),
         ))
-        rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
+        rwd_dict['dense'] = np.sum([wt * rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
         return rwd_dict
 
     def get_target_pose(self):
         if self.target_type == 'fixed':
             return self.target_pose
         elif self.target_type == 'random':
-            return self.np_random.uniform(low=self.sim.model.actuator_ctrlrange[:,0], high=self.sim.model.actuator_ctrlrange[:,1])
-
+            return self.np_random.uniform(low=self.sim.model.actuator_ctrlrange[:, 0],
+                                          high=self.sim.model.actuator_ctrlrange[:, 1])
 
     def reset(self):
         self.target_pose = self.get_target_pose()
         obs = super().reset(self.init_qpos, self.init_qvel)
         return obs
 
+
 class FrankaDmanusPoseWithBall(FrankaDmanusPose):
     DEFAULT_OBS_KEYS = [
-        'qp', 'qv', 'target_err', 'ball', 'dball' 
+        'qp', 'qv', 'target_err', 'ball', 'dball'
     ]
     DEFAULT_RWD_KEYS_AND_WEIGHTS = {
         # "pose": -1.0,
         "bonus": 4.0,
         "penalty": -50,
     }
+
     def __init__(self, model_path, target_xy_range, ball_xy_range, obsd_model_path=None, seed=None, **kwargs):
 
         self.target_xy_range = target_xy_range
         self.ball_xy_range = ball_xy_range
 
+        self.franka_njoints = 7
+        self.dmanus_njoints = 9
+        # Add code here to move sites onto palm
         super().__init__(model_path, obsd_model_path, seed, **kwargs)
-    
+
     def _setup(self,
                target_ball_pos,
                franka_init=None,
                obs_keys=DEFAULT_OBS_KEYS,
                weighted_reward_keys=DEFAULT_RWD_KEYS_AND_WEIGHTS,
                **kwargs
-        ):
-        
+               ):
+
         self.target_sid = self.sim.model.site_name2id('target')
         self.init_sid = self.sim.model.site_name2id('init_ball')
 
         self.palm_id = self.sim.model.site_bodyid[self.init_sid]
 
-        if isinstance(target_ball_pos,np.ndarray):
+        if isinstance(target_ball_pos, np.ndarray):
             self.target_type = 'fixed'
             self.target_ball_pos = target_ball_pos
         elif target_ball_pos == 'random':
             self.target_type = 'random'
-            self.target_ball_pos = self.sim.data.qpos[16:19].copy() # fake target for setup
-        
+            self.target_ball_pos = self.sim.data.qpos[16:19].copy()  # fake target for setup
 
         env_base.MujocoEnv._setup(self, obs_keys=obs_keys,
-                       weighted_reward_keys=weighted_reward_keys,
-                       frame_skip=40,
-                       **kwargs)
-        
+                                  weighted_reward_keys=weighted_reward_keys,
+                                  frame_skip=40,
+                                  **kwargs)
+
         if franka_init is not None:
             # self.sim.data.qpos[:7] = franka_init
             self.init_qpos[:7] = franka_init
@@ -174,15 +177,15 @@ class FrankaDmanusPoseWithBall(FrankaDmanusPose):
         obs_dict['t'] = np.array([self.sim.data.time])
         obs_dict['qp'] = sim.data.qpos.copy()
         obs_dict['qv'] = sim.data.qvel.copy()
-        
+
         # Add magnetometer measurements here
         obs_dict['ball'] = self.sim.data.qpos[16:19].copy()
         obs_dict['dball'] = self.sim.data.qvel[16:19].copy() * self.dt
         obs_dict['target'] = self.sim.data.site_xpos[self.target_sid].copy()
-        
+
         # Change this to only look at target pose for the ball
         obs_dict['target_err'] = obs_dict['ball'] - self.target_ball_pos
-        
+
         # Add code for mags and magdiffs
         return obs_dict
 
@@ -193,22 +196,22 @@ class FrankaDmanusPoseWithBall(FrankaDmanusPose):
         # print(f'Reach dist: {reach_dist}, far_th: {far_th}')
         rwd_dict = collections.OrderedDict((
             # Optional Keys
-            ('reach',   reach_dist),
-            ('bonus',   (reach_dist<.035) + (reach_dist<.07)),
-            ('penalty', (reach_dist>far_th)),
+            ('reach', reach_dist),
+            ('bonus', (reach_dist < .035) + (reach_dist < .07)),
+            ('penalty', (reach_dist > far_th)),
             # Must keys
-            ('sparse',  -1.0*reach_dist),
-            ('solved',  reach_dist<.03),
-            ('done',    False),
+            ('sparse', -1.0 * reach_dist),
+            ('solved', reach_dist < .03),
+            ('done', False),
             # ('done',    reach_dist > far_th),
         ))
-        rwd_dict['dense'] = np.sum([wt*rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
+        rwd_dict['dense'] = np.sum([wt * rwd_dict[key] for key, wt in self.rwd_keys_wt.items()], axis=0)
         return rwd_dict
 
     def reset(self):
         self.sim.data.qpos[:] = self.init_qpos.copy()
         self.sim.forward()
-        
+
         # Reset init and target sites
         self.sim.model.site_pos[self.target_sid][::2] = self.np_random.uniform(
             low=self.target_xy_range[0], high=self.target_xy_range[1])
@@ -220,5 +223,52 @@ class FrankaDmanusPoseWithBall(FrankaDmanusPose):
         qp = self.init_qpos.copy()
         qp[16:19] = self.sim.data.site_xpos[self.init_sid]
 
-        obs = env_base.MujocoEnv.reset(self,reset_qpos=qp)
+        obs = env_base.MujocoEnv.reset(self, reset_qpos=qp)
         return obs
+
+
+class FrankaDmanusPoseWithBallJointsRemoved(FrankaDmanusPoseWithBall):
+    DEFAULT_OBS_KEYS = [
+        'qp', 'qv', 'target_err', 'ball', 'dball'
+    ]
+    DEFAULT_RWD_KEYS_AND_WEIGHTS = {
+        # "pose": -1.0,
+        "bonus": 4.0,
+        "penalty": -50,
+    }
+
+    def __init__(self, model_path, config_path, removed_joint_ids, target_xy_range, ball_xy_range, obsd_model_path=None,
+                 seed=None, **kwargs):
+        curr_dir = os.path.dirname(os.path.abspath(__file__))
+
+        if removed_joint_ids:
+            raw_sim = env_base.get_sim(model_path=curr_dir + model_path)
+            raw_xml = raw_sim.model.get_xml()
+
+            with open(config_path, 'r') as f:
+                robot_config = eval(f.read())
+            for joint_id in removed_joint_ids:
+                joint_name = f'panda0_joint{joint_id}'
+                sensor_name = f'fr_arm_jp{joint_id}'
+
+                raw_xml = remove_joint(xml_str=raw_xml, joint_name=joint_name)
+
+                # import ipdb; ipdb.set_trace()
+                for sensor in robot_config['franka']['sensor']:
+                    if sensor['name'] == sensor_name:
+                        robot_config['franka']['sensor'].remove(sensor)
+                for actuator in robot_config['franka']['actuator']:
+                    if actuator['name'] == joint_name:
+                        robot_config['franka']['actuator'].remove(actuator)
+
+            model_path = model_path[:-4] + "_removed.xml"
+            config_path = config_path[:-7] + "_removed.config"
+
+            with open(curr_dir + model_path, 'w') as file:
+                file.write(raw_xml)
+
+            with open(config_path, 'w') as file:
+                file.write(str(robot_config))
+
+        super().__init__(model_path, target_xy_range, ball_xy_range, obsd_model_path, seed,
+                         config_path=config_path, **kwargs)
