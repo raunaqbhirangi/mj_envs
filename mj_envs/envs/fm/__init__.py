3b1fba0a815660c7c9c47692d0e4daa8d2f48fd8
Raunaq Bhirangi <raunaqbhirangi@fb.com>
2022-06-10 14:27:58 -0400

    add config file for ball balancing

--- mj_envs/envs/fm/__init__.py
+++ mj_envs/envs/fm/__init__.py
@@ -41,13 +41,13 @@ register(
 
 # Ball random target from oracle info
 register(
-    id='rpFrankaDManusBallBalanceOracle-v0',
+    id='rpFrankaDmanusBallBalanceOracle-v0',
     entry_point='mj_envs.envs.fm.franka_dmanus_pose_v0:FrankaDmanusPoseWithBall',
     max_episode_steps=100, #50steps*40Skip*2ms = 4s
     # Franka init specifies the reset joint angles
     kwargs={
             'model_path': '/assets/franka_dmanus_ball.xml',
-            'config_path': os.path.join(curr_dir,'assets/franka_dmanus.config'),
+            'config_path': os.path.join(curr_dir,'assets/franka_dmanus_ball.config'),
             'removed_joint_ids': [0,1,2,3,5],
             'obs_keys': ['qp', 'qv', 'ball', 'dball'],
             'franka_init': np.array([0.000, 1.5, 1.6, 0.0, -0.0, 0.0, -0.0]),
@@ -60,12 +60,12 @@ register(
 
 # Ball to random target from reskin data
 register(
-    id='rpFrankaDManusBallBalance-v0',
+    id='rpFrankaDmanusBallBalance-v0',
     entry_point='mj_envs.envs.fm.franka_dmanus_pose_v0:FrankaDmanusPoseWithBall',
     max_episode_steps=50, #50steps*40Skip*2ms = 4s
     kwargs={
             'model_path': '/assets/franka_dmanus_ball.xml',
-            'config_path': os.path.join(curr_dir,'assets/franka_dmanus.config'),
+            'config_path': os.path.join(curr_dir,'assets/franka_dmanus_ball.config'),
             'obs_keys': ['qp', 'qv', 'mag', 'dmag'],
             'target_xy_range': np.array(([0.0, 0.05], [0.0, 0.05])),
             'ball_xy_range': np.array(([-0.05, 0.0], [0.05, 0.1])),
